01230ddf0876f68d1e40047384c90560e0840cd5
jasaswini-mohanty <jas1234.mohanty@gmail.com>
2021-01-31 10:33:58 +0530

    added hte platform

--- sketch.js
+++ sketch.js
@@ -35,6 +35,7 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    platform= new Ground(150,330,250,200);
 }
 
 function draw(){
@@ -59,4 +60,6 @@ function draw(){
     log5.display();
 
     bird.display();
+
+    platform.display();
 }
